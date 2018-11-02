b44d9a21821b649a1dadc6f9947e71b5dbaae691
Gabriel-Batista <gabedbatista@gmail.com>
2018-11-02 16:41:45 -0400

    Adds logic in waiting page to track seats_taken

--- src/Components/WaitingPage.js
+++ src/Components/WaitingPage.js
@@ -1,14 +1,77 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { ActionCable } from "react-actioncable-provider";
 import { GameAdapters } from "../Adapters/GameAdapters";
+import GameActions from "../Actions/GameActions";
+
+import { Redirect } from "react-router-dom";
 
 class WaitingPage extends Component {
-    componentDidMount = () => {
-        GameAdapters.join().then(console.log);
+    state = {
+        redirect: false,
+        seatsTaken: 0,
+        createWebSocket: false
     };
 
+    componentDidMount = () => {
+        GameAdapters.join().then(res => {
+            if (res.error && this.props.matchId === null) {
+                this.setState({ redirect: true });
+            } else {
+                console.log("fetch:", res);
+                this.props.updateMatchId(res.id);
+                this.setState(
+                    {
+                        seatsTaken: res.seats_taken
+                    },
+                    () => this.setState({ createWebSocket: true })
+                );
+            }
+        });
+    };
+
+    handleReceived = res => {
+        console.log("ActionCable:", res);
+        this.setState({
+            seatsTaken: res.match.seats_taken
+        });
+    };
+
+    renderWaitingPage = () => (
+        <React.Fragment>
+            {this.state.createWebSocket ? (
+                <ActionCable
+                    channel={{
+                        channel: "MatchesChannel",
+                        match_id: this.props.matchId
+                    }}
+                    onReceived={this.handleReceived}
+                />
+            ) : null}
+            <p>{"waiting for " + (4 - this.state.seatsTaken) + " player/s"}</p>
+        </React.Fragment>
+    );
+
     render() {
-        return <p>waiting for players to join</p>;
+        return (
+            <div>
+                {this.state.redirect ? (
+                    <Redirect to="/" />
+                ) : (
+                    this.renderWaitingPage()
+                )}
+            </div>
+        );
     }
 }
 
-export default WaitingPage
+const mapStateToProps = state => {
+    return {
+        matchId: state.game.matchId
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    GameActions
+)(WaitingPage);
