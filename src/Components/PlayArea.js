8fda51085049c5953bbcc21472d6334c3c18b2a1
Gabriel-Batista <gabedbatista@gmail.com>
2018-11-02 20:25:36 -0400

    PlayArea renders Results on gameOver

--- src/Components/PlayArea.js
+++ src/Components/PlayArea.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import Paragraph from './Paragraph';
-import GameInput from './GameInput';
+import React from "react";
+import { connect } from "react-redux";
+import Paragraph from "./Paragraph";
+import GameInput from "./GameInput";
+import Results from "./Results";
 
 const PlayArea = props => (
     <React.Fragment>
-        <Paragraph />
-        <GameInput />
+        {props.gameOver ? (
+            <Results />
+        ) : (
+            <React.Fragment>
+                <Paragraph />
+                <GameInput />
+            </React.Fragment>
+        )}
     </React.Fragment>
 );
 
-export default PlayArea
+const mapStateToProps = state => {
+    return {
+        gameOver: state.game.gameOver
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    null
+)(PlayArea);
