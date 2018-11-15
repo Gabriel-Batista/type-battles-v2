cc5e249d5a8f2c665ae15d1b5209db02524c1fca
Gabriel-Batista <gabedbatista@gmail.com>
2018-11-15 15:27:52 -0500

    Turns users in_match to false upon logout

--- src/Components/Logout.js
+++ src/Components/Logout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Menu } from 'semantic-ui-react'
+import {UserAdapters} from '../Adapters/UserAdapters'
 
 const Logout = props => {
     const logout = () =>  {
       props.clearUser();
       props.clearGame();
       localStorage.removeItem("token")
+      UserAdapters.leaveMatch()
     }
     return <Menu.Item position="right" onClick={logout}>Logout</Menu.Item>;
 };
