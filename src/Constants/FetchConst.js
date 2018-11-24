41ba8a75fc226c0850cd83150f38e3e25d4d4bb3
Gabriel-Batista <gabedbatista@gmail.com>
2018-11-24 15:14:52 -0500

    Changes  API URL to point to hosted backend

--- src/Constants/FetchConst.js
+++ src/Constants/FetchConst.js
@@ -1,11 +1,11 @@
-// export const FetchConst = {
-//     API: "https://type-battles-v2-backend.herokuapp.com/",
-//     API_WS: "wss://type-battles-v2-backend.herokuapp.com/cable",
-//     HEADERS: { "Content-Type": "application/json", Accept: "application/json" }
-// };
-
 export const FetchConst = {
-  API: "http://localhost:3001",
-  API_WS: "ws://localhost:3001/cable",
-  HEADERS: { "Content-Type": "application/json", Accept: "application/json" }
+    API: "https://type-battles-v2-backend.herokuapp.com/",
+    API_WS: "wss://type-battles-v2-backend.herokuapp.com/cable",
+    HEADERS: { "Content-Type": "application/json", Accept: "application/json" }
 };
+
+// export const FetchConst = {
+//   API: "http://localhost:3001",
+//   API_WS: "ws://localhost:3001/cable",
+//   HEADERS: { "Content-Type": "application/json", Accept: "application/json" }
+// };
